307114173b099a7dccbacb2370977c3ef310f295
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2022-11-13 11:50:53 +0000

    Improve Resource.groovy test coverage Fixes #45

--- srcTest/net/ebdon/webdoxy/ResourceTest.groovy
+++ srcTest/net/ebdon/webdoxy/ResourceTest.groovy
@@ -1,6 +1,7 @@
 package net.ebdon.webdoxy;
 
 import groovy.test.GroovyTestCase;
+import groovy.mock.interceptor.MockFor;
 import org.apache.tools.ant.BuildException;
 import groovy.transform.TypeChecked;
 
@@ -23,13 +24,46 @@ import groovy.transform.TypeChecked;
  * limitations under the License.
  */
 
+@groovy.util.logging.Log4j2('logger')
 class ResourceTest extends GroovyTestCase {
 
-  private final String goodMsgKey = 'backup.alreadyExists'
-  private final String badMsgKey  = 'xyzzy'
+  private final String goodMsgKey     = 'backup.alreadyExists';
+  private final String goodMsgKeyArgs = 'project.stub.creatingToDo';
+  private final String badMsgKey      = 'xyzzy';
+  private final String[] args         = ['wibble', 'wobble'];
+
+  private MockFor bundleMock;
+
+  private final ResourceBundle dummyResourceBundle = new ResourceBundle() {
+    private int numGetStringCalls = 0
+
+    @TypeChecked
+    @Override protected Object handleGetObject(String key) {
+      ++numGetStringCalls
+      "${numGetStringCalls}: ResourceBundle.getString( $key )".toString()
+    }
+
+    @TypeChecked
+    @Override Enumeration<String> getKeys() {
+      Collections.emptyEnumeration();
+    }
+
+    @TypeChecked
+    final int getNumGetStringCalls() {
+      this.numGetStringCalls
+    }
+  }
+
+  @TypeChecked
+  @Override void setUp() {
+    super.setUp()
+    logger.trace 'Test setup'
+    bundleMock = new MockFor( ResourceBundle )
+  }
 
   @TypeChecked
   void testMessageBadKey() {
+    logger.info 'Start of testMessageBadKey()'
     shouldFail( BuildException ) {
       try {
         new Resource().message( badMsgKey )
@@ -43,9 +77,81 @@ class ResourceTest extends GroovyTestCase {
 
   @TypeChecked
   void testMessageGoodKey() {
+    logger.info 'Start of testMessageGoodKey()'
     final String msg = new Resource().message( goodMsgKey )
 
     assert msg?.length()
-    assert !msg?.contains( goodMsgKey )
+    assert !msg.contains( goodMsgKey )
+  }
+
+  @TypeChecked
+  void testMessageGoodKeyArgs() {
+    logger.info 'Start of testMessageGoodKeyArgs()'
+    final Resource res = new Resource()
+    final String msg = res.message( goodMsgKeyArgs, args )
+
+    assert msg?.length()
+    assert !msg.contains( goodMsgKey )
+    args.each { final String arg ->
+      assert msg.contains( arg )
+    }
+  }
+
+  @TypeChecked
+  void testMessageBadKeyArgs() {
+    logger.info 'Start of testMessageBadKeyArgs()'
+    shouldFail( BuildException ) {
+      try {
+        new Resource().message( badMsgKey, args )
+      } catch ( final BuildException bex ) {
+        assert bex.message.contains( badMsgKey )
+        assert bex.message.contains( 'Can\'t find resource for bundle' )
+        throw bex
+      }
+    }
+  }
+
+  void testLoadMissingBundle() {
+    final String expectedBundleName = 'Language'
+    logger.info 'Start of testLoadMissingBundle()'
+
+    bundleMock.demand.getBundle { final String name ->
+      logger.info "getBundle called with name: $name"
+      assert expectedBundleName == 'Language'
+      throw new MissingResourceException(
+        expectedBundleName, this.class.name, name
+      )
+    }
+
+    bundleMock.use {
+      final Resource res = new Resource()
+      shouldFail( BuildException ) {
+        try {
+          res.message( goodMsgKey )
+        } catch ( final BuildException bex ) {
+          assert bex.message.contains( expectedBundleName )
+          throw bex
+        }
+      }
+      assert dummyResourceBundle.numGetStringCalls == 0
+    }
+  }
+
+  void testLoadBundle() {
+    logger.info 'Start of testLoadBundle()'
+
+    bundleMock.demand.getBundle { final String name ->
+      logger.info "getBundle called with name: $name"
+      assert name == 'Language'
+      dummyResourceBundle
+    }
+
+    bundleMock.use {
+      final Resource res = new Resource()
+      1.upto(2) { // 1: not loaded, 2: already loaded
+        res.message( goodMsgKey )
+        assert dummyResourceBundle.numGetStringCalls == it
+      }
+    }
   }
 }
